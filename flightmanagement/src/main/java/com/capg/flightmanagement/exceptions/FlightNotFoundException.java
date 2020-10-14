e8f6e1d3c4d43fe865d4898044b67b012b536e0c
Sheikh Abdullah <abdullahqidwae@gmail.com>
2020-10-14 11:42:17 +0530

    commit

--- flightmanagement/src/main/java/com/capg/flightmanagement/exceptions/FlightNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.capg.flightmanagement.exceptions;
-
-public class FlightNotFoundException extends RuntimeException{
-	
-	public FlightNotFoundException(String msg) {
-		super(msg);
-	}
-
-	}
